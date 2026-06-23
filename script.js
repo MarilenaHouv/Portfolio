585d81cf03e3e4407b87c42faa6f69f2d85d6f12
Eleni Houvarda <eleni.mariah04@gmail.com>
2026-06-23 19:57:50 -0400

    added draft projects page, made more style edits to about page, switched to popup window style, changed picture for socials

--- script.js
+++ script.js
@@ -5,9 +5,3 @@ document.addEventListener('DOMContentLoaded', function() {
     document.documentElement.style.scrollBehavior = 'smooth';
 
 });
-
-const navLinks = document.querySelector('.nav-links');
-function onToggleMenu(e){
-    e.name = e.name === "menu" ? "close" : "menu";
-    navLinks.classList.toggle('top-[9%]')
-}
